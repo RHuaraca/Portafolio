0c854486a46c322e06fa45e5c303f4063fce528e
Rene Huaraca <renepersonal931@gmail.com>
2023-03-28 15:16:41 -0500

    indicador de posicion en la barra de navegacion

--- rene-portafolio/src/components/NavBarInicio.jsx
+++ rene-portafolio/src/components/NavBarInicio.jsx
@@ -1,6 +1,6 @@
 import foto from "../assets/perfilOriginal.jpeg";
 import iconoMenu from "../assets/iconomenu.png";
-import { Link, useLocation } from "react-router-dom";
+import { useLocation, NavLink } from "react-router-dom";
 import { useState } from "react";
 import { useSelector } from "react-redux";
 
@@ -17,15 +17,17 @@ export default function NavBarInicio(){
     return (
         <nav className={modoActual==="oscuro"?"navbar_container":"navbar_container_lightMode"}>
             <div className="navbar_imageContainer">
-                <Link to="/About"><img src={foto}  alt="foto de perfil" className={location.pathname==="/Home"?`perfilInicio`:location.pathname==="/About"?'perfilAbout':'perfilOthers'}/></Link>
+                <NavLink to="/About" activeClassName="active">
+                    <img src={foto}  alt="foto de perfil" className={location.pathname==="/Home"?`perfilInicio`:location.pathname==="/About"?'perfilAbout':'perfilOthers'}/>
+                </NavLink>
                 <img src={iconoMenu} alt="menu" className={location.pathname==="/Home"?"menuImgInicio":"menuImgOthers"} onClick={()=>ocultar()}/>   
             </div>
             <ul className={menu}>
                 {opciones.map((opcion,i)=>(
                     <li key={i} onClick={()=>ocultar()}>
-                        <Link to={location.pathname!==opcionesIngles[i]?`/${opcionesIngles[i]}`:null} className="enlace">
+                        <NavLink to={location.pathname!==opcionesIngles[i]?`/${opcionesIngles[i]}`:null} className="enlace">
                             {opcion}
-                        </Link>
+                        </NavLink>
                     </li>
                 ))}
             </ul>
