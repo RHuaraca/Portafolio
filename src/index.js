5c9b08f994e43dd5b531cfc4932d48e10a3b4114
Rene Huaraca <renepersonal931@gmail.com>
2023-04-13 19:42:38 -0500

    correccion de errores 0.5

--- src/index.js
+++ src/index.js
@@ -3,7 +3,8 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import "./styles/styles.scss";
-import { BrowserRouter } from "react-router-dom";
+//import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 import { Provider } from "react-redux";
 import { store, persistor } from "./redux/store.js";
 import { PersistGate } from "redux-persist/es/integration/react";
@@ -13,9 +14,9 @@ root.render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <BrowserRouter>
+        <Router basename={process.env.PUBLIC_URL}>
           <App />
-        </BrowserRouter>
+        </Router>
       </PersistGate>
     </Provider>
   </React.StrictMode>
